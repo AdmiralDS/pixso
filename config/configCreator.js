70047143f7f76b302a2e98479d21518f7c9ef734
Andre Diatkov <andyatkov@gmail.com>
2025-01-28 01:57:45 +0300

    [Feature] - Add CLI command

--- config/configCreator.js
+++ config/configCreator.js
@@ -14,79 +14,86 @@ async function askQuestion(query) {
 async function createConfigFile() {
     console.log("Setting up configuration for file generation...");
 
-    const platform = await askQuestion("Enter platform (e.g., WEB, MOBILE, IOS, ANDROID): ");
-    const outputFilePath = await askQuestion("Enter the output file path with file extention, like color.dark.ts or color.dark.swift: ");
-    const items = [];
+    const themes = [];
+    let addMoreTheme = true;
+    while (addMoreTheme) {
+        const platform = await askQuestion("Enter platform (e.g., WEB, MOBILE, IOS, ANDROID): ");
+        const outputFilePath = await askQuestion("Enter the output file path with file extention, like color.dark.ts or color.dark.swift: ");
+        const items = [];
 
-    let addMoreItems = true;
-    while (addMoreItems) {
-        console.log("\nAdding a new item...");
+        let addMoreItems = true;
+        while (addMoreItems) {
+            console.log("\nAdding a new item...");
 
-        const type = await askQuestion("Enter item type (COLOR or SHADOW): ");
-        const name = await askQuestion("Enter name (e.g., color or boxShadow): ");
-        const apiBaseUrl = await askQuestion("Enter API URL (e.g., https://pixso.t1-pixso.ru): ");
-        const fileKey = await askQuestion("Enter fileKey: ");
-        const templatePath = await askQuestion("Enter template path (leave empty if not needed): ");
-        
-        const removePercent = await askQuestion("Remove percentage from names? (true/false): ");
-        const sort = await askQuestion("Sort items? (true/false): ");
+            const type = await askQuestion("Enter item type (COLOR or SHADOW): ");
+            const name = await askQuestion("Enter name (e.g., color or boxShadow): ");
+            const apiBaseUrl = await askQuestion("Enter API URL (e.g., https://pixso.t1-pixso.ru): ");
+            const fileKey = await askQuestion("Enter fileKey: ");
+            const templatePath = await askQuestion("Enter template path (leave empty if not needed): ");
 
-        console.log("\nChoose a naming convention:");
-        console.log("1 - camelCase");
-        console.log("2 - PascalCase");
-        console.log("3 - snake_case");
-        console.log("4 - kebab-case");
-        console.log("5 - flatcase");
+            const removePercent = await askQuestion("Remove percentage from names? (true/false): ");
+            const sort = await askQuestion("Sort items? (true/false): ");
 
-        let namingConvention;
-        while (!namingConvention) {
-            const namingChoice = await askQuestion("Enter your choice (1-4): ");
-            switch (namingChoice) {
-                case "1":
-                    namingConvention = "camelCase";
-                    break;
-                case "2":
-                    namingConvention = "PascalCase";
-                    break;
-                case "3":
-                    namingConvention = "snake_case";
-                    break;
-                case "4":
-                    namingConvention = "kebab-case";
-                    break;
-                case "5":
-                    namingConvention = "flatcase";
-                    break;
-                default:
-                    console.log("Invalid choice, please select a valid option (1-5).");
+            console.log("\nChoose a naming convention:");
+            console.log("1 - camelCase");
+            console.log("2 - PascalCase");
+            console.log("3 - snake_case");
+            console.log("4 - kebab-case");
+            console.log("5 - flatcase");
+
+            let namingConvention;
+            while (!namingConvention) {
+                const namingChoice = await askQuestion("Enter your choice (1-4): ");
+                switch (namingChoice) {
+                    case "1":
+                        namingConvention = "camelCase";
+                        break;
+                    case "2":
+                        namingConvention = "PascalCase";
+                        break;
+                    case "3":
+                        namingConvention = "snake_case";
+                        break;
+                    case "4":
+                        namingConvention = "kebab-case";
+                        break;
+                    case "5":
+                        namingConvention = "flatcase";
+                        break;
+                    default:
+                        console.log("Invalid choice, please select a valid option (1-5).");
+                }
             }
+
+            items.push({
+                type,
+                name,
+                apiBaseUrl,
+                fileKey,
+                ...(templatePath ? { templatePath } : {}),
+                transformRules: {
+                    removePercent: removePercent.toLowerCase() === "true",
+                    sort: sort.toLowerCase() === "true",
+                    namingConvention
+                }
+            });
+
+            const addAnother = await askQuestion("Add another item? (yes/no): ");
+            addMoreItems = addAnother.toLowerCase() === "yes";
         }
 
-        items.push({
-            type,
-            name,
-            apiBaseUrl,
-            fileKey,
-            ...(templatePath ? { templatePath } : {}),
-            transformRules: {
-                removePercent: removePercent === "true",
-                sort: sort === "true",
-                namingConvention
-            }
+        themes.push({
+            platform,
+            outputFilePath,
+            items
         });
 
-        const addAnother = await askQuestion("Add another item? (yes/no): ");
-        addMoreItems = addAnother.toLowerCase() === "yes";
+        const addAnotherTheme = await askQuestion("Add another theme? (yes/no): ");
+        addMoreTheme = addAnotherTheme.toLowerCase() === "yes";
     }
 
-    const configData = [{
-        platform,
-        outputFilePath,
-        items
-    }];
-
     const configPath = path.join(__dirname, 'config.json');
-    fs.writeFileSync(configPath, JSON.stringify(configData, null, 2), 'utf8');
+    fs.writeFileSync(configPath, JSON.stringify(themes, null, 2), 'utf8');
 
     console.log(`\n✅ Configuration file successfully created: ${configPath}`);
     rl.close();
--- main.js
+++ main.js
@@ -1,65 +1,22 @@
-const fs = require('fs');
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
 const path = require('path');
-const { createConfigFile } = require('./config/configCreator');
-const { loadConfig } = require('./config/configLoader');
-const { getToken } = require('./auth/auth');
-const { clearFile } = require('./file/fileUtils');
-const { getColors } = require('./handler/colorHandler');
-const { getShadows } = require('./handler/shadowHandler');
-const { getDefaultColorTemplatePath, getDefaultShadowTemplatePath } = require('./config/defaultTemplate');
+const { main } = require('./mainLogic');
 
-async function main() {
-    const configPath = path.join(__dirname, 'config/config.json');
-
-    if (!fs.existsSync(configPath)) {
-        console.log("The config file.json was not found. Starting the setup...");
-        await createConfigFile();
-    }
-
-    let configs;
-    try {
-        configs = await loadConfig(configPath);
-    } catch (err) {
-        console.error('Error reading or parsing the configuration file:', err);
-        return;
-    }
-
-    for (const config of configs) {
-        if (!config.outputFilePath) continue;
-
-        clearFile(config.outputFilePath);
-
-        for (const item of config.items || []) {
-            await processItem(item, config);
+const argv = yargs(hideBin(process.argv))
+    .command('run', 'Run the script with an existing configuration', {}, async () => {
+        try {
+            await main();
+            console.log('✅ Success update.');
+        } catch (error) {
+            console.error('❌ Error command:', error.message);
+            process.exit(1);
         }
-    }
-}
-
-async function processItem(item, config) {
-    const baseURL = item.apiBaseUrl || '';
-    const fileKey = item.fileKey || '';
-    const type = item.type || '';
-    const name = item.name || '';
-    const templateColorPath = item.templatePath || getDefaultColorTemplatePath(__dirname, config.platform);
-    const templateShadowPath = item.templatePath || getDefaultShadowTemplatePath(__dirname, config.platform);
-
-    if (!baseURL || !fileKey || !type || !name) {
-        console.error('Error: Missing required properties');
-        return;
-    }
-
-    let token = "";
-    try {
-        token = await getToken(baseURL);
-    } catch (err) {
-        console.error('Can not get token:', err.message);
-    }
-
-    if (type === "COLOR") {
-        await getColors(baseURL, fileKey, name, token, templateColorPath, config.outputFilePath, item.transformRules);
-    } else if (type === "SHADOW") {
-        await getShadows(baseURL, fileKey, name, token, templateShadowPath, config.outputFilePath, item.transformRules);
-    }
-}
-
-main()
+        process.exit(0);
+    })
+    .demandCommand(1, 'You need to specify a command (setup or run).')
+    .help()
+    .alias('help', 'h')
+    .argv;
