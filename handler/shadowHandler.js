2afdcf0043d2eab8eac1af36285f85cd01a9ff55
Andyatkov <andyatkov@gmail.com>
2025-02-07 10:05:31 +0300

    [Feature] - Добавили получение json токенов

--- handler/shadowHandler.js
+++ handler/shadowHandler.js
@@ -45,4 +45,43 @@ async function getShadows(baseURL, fileKey, name, token, templatePath, outputFil
     }
 }
 
-module.exports = { getShadows };
+async function getJSONShadows(baseURL, fileKey, name, token, templatePath, outputFilePath, transformRules) {
+    try {
+        const shadows = await getMapShadows(baseURL, fileKey, token);
+
+        const rules = parseRules(transformRules);
+
+        var transformedShadows = shadows.map(shadow => {
+            let transformedShadowName = shadow.name;
+            rules.forEach(func => {
+                transformedShadowName = func(transformedShadowName);
+            });
+            return { 
+                name: transformedShadowName, 
+                values: shadow.values.map(value => ({
+                    x: value.x,
+                    y: value.y,
+                    spread: value.spread,
+                    radius: value.radius,
+                    red: value.red,
+                    green: value.green,
+                    blue: value.blue,
+                    alpha: value.alpha,
+                    isShowInset: value.isShowInset,
+                }))
+            };
+        });
+
+        if (transformRules.sort == true) {
+            transformedShadows = transformedShadows.sort((a, b) => a.name.localeCompare(b.name));
+        }
+
+        const jsonOutput = JSON.stringify(transformedShadows, null, 2);
+        console.log(jsonOutput);
+        return jsonOutput;
+    } catch (err) {
+        console.error('Error:', err.message);
+    }
+}
+
+module.exports = { getShadows, getJSONShadows };
--- main.js
+++ main.js
@@ -3,13 +3,23 @@
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 const path = require('path');
-const { main } = require('./mainLogic');
+const { main, getMainJSON } = require('./mainLogic');
 
 const argv = yargs(hideBin(process.argv))
     .command('run', 'Run the script with an existing configuration', {}, async () => {
         try {
             await main();
             console.log('✅ Success update.');
+        } catch (error) {
+            console.error('❌ Error command:', error.message);
+            process.exit(1);
+        }
+        process.exit(0);
+    })
+    .command('json', 'Run the script to get json', {}, async () => {
+        try {
+            await getMainJSON();
+            console.log('✅ Success get json.');
         } catch (error) {
             console.error('❌ Error command:', error.message);
             process.exit(1);
